835edc738ba617923874c2cc28b25a563c39995c
jlugo5 <45322802+jlugo5@users.noreply.github.com>
2022-06-22 13:46:30 -0400

    Update oophw.py

--- oophw.py
+++ oophw.py
@@ -7,16 +7,19 @@
     # Slope fotmula
     # m = (Y2 - Y1) / (X2- X1)
 
+import math
+    
 class Line:
     
     def __init__(self,coor1,coor2):
-        pass
+        self.coor1 = coor1
+        self.coor2 = coor2
     
     def distance(self):
-        pass
+        return math.sqrt( (self.coor2[0] - self.coor1[0])**2 + (self.coor2[1] - self.coor1[1])**2 )
     
     def slope(self):
-        pass
+        return ( (self.coor2[1] - self.coor1[1]) / (self.coor2[0] - self.coor1[0]) )
 
 coordinate1 = (3,2)
 coordinate2 = (8,10)
@@ -38,14 +41,17 @@ li.slope()
 
 class Cylinder:
     
+    pi = 3.14
+    
     def __init__(self,height=1,radius=1):
-        pass
+        self.height = height
+        self.radius = radius
         
     def volume(self):
-        pass
+        return ( pi * self.radius**2 * self.hieght )
     
     def surface_area(self):
-        pass
+        return ( 2 * pi * self.radius * self.height + 2 * pi * self.radius**2 )
 
 
 c = Cylinder(2,3)
